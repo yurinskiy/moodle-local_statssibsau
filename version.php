77960465fdb06304690a89deea2edeee1b688772
Yuriy Yurinskiy <yuriyyurinskiy@yandex.ru>
2020-04-05 14:14:43 +0700

    chore: версионирование 0.0.7

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2020032200;
+$plugin->version   = 2020040500;
 $plugin->requires  = 2018120300;
 $plugin->component = 'local_statssibsau';
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = '0.0.6';
+$plugin->release = '0.0.7';
